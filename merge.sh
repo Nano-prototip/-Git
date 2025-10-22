1772dd9caefde9d5a2417be2da18e8a2005b889d
Nano-prototip <nano-prototip@yandex.ru>
2025-10-22 17:01:26 +0300

    Update merge.sh

--- merge.sh
+++ merge.sh
@@ -2,8 +2,8 @@ echo '#!/bin/bash
 # display command line options
 
 count=1
-while [[ -n "$1" ]]; do
-    echo "Parameter #$count = $1"
+while [[ -n "$@" ]]; do
+    echo "Parameter #$count = $@"
     count=$(( $count + 1 ))
     shift
 done' > merge.sh
